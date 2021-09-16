bf565677bc07914168f6c477b729ce8340be3fc4
Keith Alcock <github@keithalcock.com>
2021-09-16 09:01:11 -0700

    Tail recurse

--- src/main/scala/org/clulab/torchscript/NerDataset.scala
+++ src/main/scala/org/clulab/torchscript/NerDataset.scala
@@ -4,38 +4,38 @@ import org.clulab.torchscript.utils.Closer.AutoCloser
 import org.clulab.torchscript.utils.StringUtils
 import org.pytorch.Tensor
 
+import scala.annotation.tailrec
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
 class NerDataset(datasetPath: String, val vocab: Map[String, Int]) extends IndexedSeq[(Tensor, Tensor)] {
 
-  case class Sample(tokens: Array[String], labels: Array[String])
+  protected def getSamples(lines: Iterator[String]): Array[NerDataset.Sample] = {
 
-  object Sample {
-    def apply(tokensAndLabels: Array[(String, String)]): Sample = {
-      val (tokens, labels) = tokensAndLabels.unzip
-      Sample(tokens, labels)
-    }
-  }
+    @tailrec
+    def recGetSamples(arrayBuffer: ArrayBuffer[NerDataset.Sample]): ArrayBuffer[NerDataset.Sample] = {
+      if (lines.isEmpty) arrayBuffer
+      else {
+        val tokensAndLabels = lines.takeWhile(_ != " ").toArray.map { line =>
+          (
+            StringUtils.beforeFirst(line, ' ', all = true),
+            StringUtils.afterLast(line, ' ', all = false)
+          )
+        }
 
-  val samples = Source.fromFile(datasetPath).autoClose { source =>
-    val samples = new ArrayBuffer[Sample]()
-    val lines = source.getLines
-
-    while (lines.nonEmpty) {
-      val sampleLines = lines.takeWhile(_ != " ").toArray
-      val tokensAndLabels = sampleLines.map { line =>
-        (
-          StringUtils.beforeFirst(line, ' ', true),
-          StringUtils.afterLast(line, ' ', false)
-        )
+        arrayBuffer.append(NerDataset.Sample(tokensAndLabels))
+        recGetSamples(arrayBuffer)
       }
-      samples += Sample(tokensAndLabels)
     }
-    samples.toArray
+
+    recGetSamples(new ArrayBuffer[NerDataset.Sample]).toArray
   }
-  val labels = {
+
+  protected val samples: Array[NerDataset.Sample] = Source.fromFile(datasetPath).autoClose { source =>
+    getSamples(source.getLines)
+  }
+  protected val labels: mutable.Map[String, Int] = {
     val labels = mutable.Map.empty[String, Int]
 
     samples.foreach { sample =>
@@ -62,5 +62,17 @@ class NerDataset(datasetPath: String, val vocab: Map[String, Int]) extends Index
       Tensor.fromBlob(tokenIndexes, Array(1L, tokenIndexes.length.toLong)),
       Tensor.fromBlob(labelIndexes, Array(1L, labelIndexes.length.toLong))
     )
+  }
+}
+
+object NerDataset {
+
+  case class Sample(tokens: Array[String], labels: Array[String])
+
+  object Sample {
+    def apply(tokensAndLabels: Array[(String, String)]): Sample = {
+      val (tokens, labels) = tokensAndLabels.unzip
+      Sample(tokens, labels)
+    }
   }
 }
