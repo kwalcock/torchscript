9d5edb36829abd3ea9492580dba5daf2e9220d0f
Keith Alcock <github@keithalcock.com>
2021-09-23 09:38:07 -0700

    Separate out python onnx code

--- src/main/python/run_onnx.py
+++ src/main/python/run_onnx.py
@@ -11,7 +11,7 @@ from Shared import make_model, run_dataset
 def main():
     datapath = "../../../../data/ner-conll/"
     embedpath = "../../../../data/glove.840B.300d.10f.txt"
-    modelpath = "../../../../data/model.pt"
+    modelpath = "../../../../data/model.onnx"
     datamodule = NerDatamodule(datapath, embedpath, batch_size = 1)
     example_crop = 50
     crop = 0
@@ -20,13 +20,15 @@ def main():
     print(f"Size of vocab: {len(datamodule.train_dataset.vocab)}")
 
     def save_model(model, input):
-        model.save(modelpath)
-        print("The torchscript model was saved.")
+        input_names = [ "input" ]
+        output_names = [ "output" ]
+        # Why does this issue a warning?
+        torch.onnx.export(model, example_input, onnxpath, verbose = True, input_names = input_names, output_names = output_names)
+        print("The onnx model was saved.")
 
     def make_scripted_model(size_of_vocab, size_of_labels):
-        if os.path.isfile(modelpath):
-            # If the file already exists, just load it.
-            model = torch.jit.load(modelpath)
+        if False:
+            # Don't bother to load it.
         else:
             example_input = torch.randint(1, size_of_vocab, (1, example_crop))
             model = make_model(size_of_vocab, size_of_labels)
@@ -37,9 +39,10 @@ def main():
     model = make_scripted_model(len(datamodule.train_dataset.vocab), datamodule.num_classes)
 
     with torch.no_grad():
-        run_dataset(model, "train", crop, datamodule.train_dataloader())
-        run_dataset(model, "  val", crop, datamodule.val_dataloader())
-        run_dataset(model, " test", crop, datamodule.test_dataloader())
+        # Don't run the onnx model here
+        # run_dataset(model, "train", crop, datamodule.train_dataloader())
+        # run_dataset(model, "  val", crop, datamodule.val_dataloader())
+        # run_dataset(model, " test", crop, datamodule.test_dataloader())
 
 
 if __name__ == "__main__":
